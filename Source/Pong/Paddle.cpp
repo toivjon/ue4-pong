d8509c1387bc3301e536dfc2712a980eb3783870
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-03-04 21:37:25 +0200

    Added a base class for the ScoreIndicator and some other comment fixes.

--- Source/Pong/Paddle.cpp
+++ Source/Pong/Paddle.cpp
@@ -11,7 +11,7 @@ APaddle::APaddle(const FObjectInitializer& objectInitializer) : APawn(objectInit
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-  // get a reference on the wall sprite from the sprites directory.
+  // get a reference on the sprite from the sprites directory.
   ConstructorHelpers::FObjectFinder<UPaperSprite> sprite(TEXT("PaperSprite'/Game/Sprites/Paddle.Paddle'"));
 
   // construct and attach a new sprite component with the loaded sprite.
--- Source/Pong/ScoreIndicator.cpp
+++ Source/Pong/ScoreIndicator.cpp
@@ -1,30 +1,30 @@
 // Copyright 2018 Joni Toiviainen All Rights Reserved.
 
-#include "Ball.h"
+#include "ScoreIndicator.h"
 
 #include "ConstructorHelpers.h"
 #include "PaperSprite.h"
 #include "PaperSpriteComponent.h"
 
-ABall::ABall(const FObjectInitializer& objectInitializer) : AActor(objectInitializer)
+AScoreIndicator::AScoreIndicator(const FObjectInitializer& objectInitializer) : AActor(objectInitializer)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
   // get a reference on the sprite from the sprites directory.
-  ConstructorHelpers::FObjectFinder<UPaperSprite> sprite(TEXT("PaperSprite'/Game/Sprites/Ball.Ball'"));
+  ConstructorHelpers::FObjectFinder<UPaperSprite> sprite(TEXT("PaperSprite'/Game/Sprites/Zero.Zero'"));
 
   // construct and attach a new sprite component with the loaded sprite.
   mSprite = objectInitializer.CreateDefaultSubobject<UPaperSpriteComponent>(this, TEXT("SpriteComponent"));
   mSprite->SetSprite(sprite.Object);
 }
 
-void ABall::BeginPlay()
+void AScoreIndicator::BeginPlay()
 {
-	Super::BeginPlay();
+	Super::BeginPlay();	
 }
 
-void ABall::Tick(float DeltaTime)
+void AScoreIndicator::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
